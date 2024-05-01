7cfbd3bfa1cf4b4c538fcd9722a563b22abaaf08
AntCannon <anthonycannonier@gmail.com>
2024-04-30 21:25:39 -0400

    add sumNums()

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-function sumNums(...c) {
-  if (!c.every((num) => Number.isNumber(num))) throw "All arguments must be numbers.";
-  return c.reduce((a, b) => a + b);
+function sumNums(...nums) {
+  if (!nums.every((num) => Number.isInteger(num))) {
+    throw "All arguments must be numbers.";
+  } 
+    
+  return nums.reduce((a, b) => a + b);
 }
 
 console.log(sumNums(1, 2, 3)); //> 6
+console.log(sumNums(10, 20, 30)); //> 60
 sumNums(10, "B", 20); //> error All arguments must be numbers.
+console.log(sumNums(10, 20, 30)); //> 60
