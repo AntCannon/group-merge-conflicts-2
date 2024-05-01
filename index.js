6f1341005516df8ff57a7187f468994139f945b9
AntCannon <anthonycannonier@gmail.com>
2024-04-30 21:16:17 -0400

    start update function

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-function fn(...c) {
-  if (!c.every((a) => Number.isNumber(a)))
-    throw "All arguments must be numbers.";
+function sumNums(...c) {
+  if (!c.every((num) => Number.isNumber(num))) throw "All arguments must be numbers.";
   return c.reduce((a, b) => a + b);
 }
 
-fn(1, 2, 3); //> 6
-fn(10, "B", 20); //> error All arguments must be numbers.
+console.log(sumNums(1, 2, 3)); //> 6
+sumNums(10, "B", 20); //> error All arguments must be numbers.
